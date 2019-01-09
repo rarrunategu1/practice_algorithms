9bdfc7c3a89e0e01474999d999b9ae0af6a01155
Rose Parker <parker.rose@ymail.com>
2019-01-09 03:25:01 +0000

    boardBuild iterates through characters in an array and prints them out.

--- draw_board.c
+++ draw_board.c
@@ -1,7 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
+char drawBoard();
+
 int main(void)
 {
-    printf("hello, world\n");
+    char letsPlay = drawBoard();
+
+}
+
+char drawBoard()
+{
+    char boardBuild[3] = {'+', '-', '|'};
+    int i;
+
+    for (i = 0; i < 3; i++)
+
+    {
+        printf("%c", boardBuild[i]);
+    }
+    printf("\n");
+    return '_';
 }
