b1d5eec55cb196337709cfd4879d2d3af4933df0
Rose Parker <parker.rose@ymail.com>
2019-01-23 04:14:13 +0000

    Able to print out board but not still need to print out third character of each row.

--- drawBoard/draw_board.c
+++ drawBoard/draw_board.c
@@ -1,35 +1,72 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+#define ROW 5
+#define COLUMN 2
+#define FIRSTPLAYER 1
+#define SECONDPLAYER 2
+#define XMOVE X
+#define YMOVE Y
 
 char drawBoard();
+void importBoard (string[]);
+
+char empty = ' ';
+char ex = 'X';
+char oh = 'O';
+
+char drawboard[9];
+
 
 int main(void)
 {
-    char letsPlay = drawBoard();
 
+    char letsPlay = drawBoard();
 
 
 }
 
 char drawBoard()
 {
+
     int i;
     int j;
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < ROW; i++)
     {
-        for(j = 0; j < 2; j++)
+        for(j = 0; j < COLUMN; j++)
         {
-            if(i % 2 == 0)
+
+            if (i % 2 == 0 && j == 0) {
+                    drawboard[0] = 'X';
+                    putchar(drawboard[0]);
+                }
+            else if (i % 2 == 0 && j == 1)
             {
-                putchar('X');
+
+                    drawboard[1] = 'O';
+                    putchar(drawboard[1]);
+            }
+            else if (i % 2 == 0 && j == 2)
+            {
+
+                    drawboard[2] = 'W';
+                    putchar(drawboard[2]);
+            }
+
+           if(i % 2 == 0)
+
+            {
+                //putchar(' ');
                 putchar('|' + (j/2));
 
                 if (j == 1)
                 {
-                    putchar('O');
+                    //putchar(' ');
                     printf("\n");
                 }
+
             }
             if(i % 2 == 1)
             {
