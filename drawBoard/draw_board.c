e608dd5dee418cdeca97041fc7fc437ab3d126f7
Rose Parker <parker.rose@ymail.com>
2019-01-25 00:30:23 +0000

    able to print drawboard with boardplaces and assign either an X, empty space or an O in it

--- drawBoard/draw_board.c
+++ drawBoard/draw_board.c
@@ -6,8 +6,6 @@
 #define COLUMN 2
 #define FIRSTPLAYER 1
 #define SECONDPLAYER 2
-#define XMOVE X
-#define YMOVE Y
 
 char drawBoard();
 void importBoard (string[]);
@@ -16,13 +14,15 @@ char empty = ' ';
 char ex = 'X';
 char oh = 'O';
 
-char drawboard[9];
+char boardPlace[9] = {'0','1','2','3','4','5','6','7','8'};
 
 
 int main(void)
 {
+    boardPlace[0] = ex;
 
     char letsPlay = drawBoard();
+
 
 
 }
@@ -33,60 +33,33 @@ char drawBoard()
     int i;
     int j;
 
-    for (i = 0; i < ROW; i++)
+    for (i = 0; i <= 4; i++)
     {
-        for(j = 0; j < COLUMN; j++)
+        for(j = 0; j < 1; j++)
         {
 
-            if (i % 2 == 0 && j == 0) {
-                    drawboard[0] = 'X';
-                    putchar(drawboard[0]);
-                }
-            else if (i % 2 == 0 && j == 1)
+            if (i == 0)
             {
-
-                    drawboard[1] = 'O';
-                    putchar(drawboard[1]);
+                printf("%c|%c|%c \n", boardPlace[0], boardPlace[1], boardPlace[2]);
             }
-            else if (i % 2 == 0 && j == 2)
+            else if (i % 2 == 1)
             {
-
-                    drawboard[2] = 'W';
-                    putchar(drawboard[2]);
+                printf("-+-+-\n");
             }
-
-           if(i % 2 == 0)
-
+            else if (i == 2)
             {
-                //putchar(' ');
-                putchar('|' + (j/2));
-
-                if (j == 1)
-                {
-                    //putchar(' ');
-                    printf("\n");
-                }
-
+                printf("%c|%c|%c \n", boardPlace[3], boardPlace[4], boardPlace[5]);
             }
-            if(i % 2 == 1)
+            else if (i == 4)
             {
-                putchar('-' + (j/2));
-                putchar('+' + (j/2));
-
-                if(j == 1)
-                {
-                    putchar('-');
-                    printf("\n");
-                }
-
+                printf("%c|%c|%c \n", boardPlace[6], boardPlace[7], boardPlace[8]);
             }
+
+
 
         }
 
     }
 
-
-
-    printf("\n");
     return '_';
 }
