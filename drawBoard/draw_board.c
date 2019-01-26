f47c71a3118c7d5bbfc8c3237d1c40e3159d66b4
Rose Parker <parker.rose@ymail.com>
2019-01-26 02:21:43 +0000

    new features asks player 1 to pick a position for their move, then it asks for player 2. Need to check if boardPlace is taken by an X or O character in a different place

--- drawBoard/draw_board.c
+++ drawBoard/draw_board.c
@@ -7,21 +7,58 @@
 #define FIRSTPLAYER 1
 #define SECONDPLAYER 2
 
+
 char drawBoard();
 void importBoard (string[]);
 
-char empty = ' ';
-char ex = 'X';
-char oh = 'O';
 
 char boardPlace[9] = {'0','1','2','3','4','5','6','7','8'};
 
 
 int main(void)
 {
-    boardPlace[0] = ex;
+    char firstPlayerMove;
+    char secondPlayerMove;
+    int i = 0;
 
-    char letsPlay = drawBoard();
+    //do until board is filled with X's and O's or game is won
+    do
+    {
+        firstPlayerMove = get_char("Player 1 enter an unused number position from 0-8 to place your move: ");
+
+    }
+    while ((firstPlayerMove < 48 || firstPlayerMove > 56 )&& (boardPlace[i] == 'X' || boardPlace[i] == 'O'));
+
+    for(i = 0; i < 9; i++)
+    {
+        if(boardPlace[i] == firstPlayerMove)
+        {
+            boardPlace[i] = 'X';
+        }
+
+    }
+
+       char letsPlay = drawBoard();
+
+        do
+    {
+        secondPlayerMove = get_char("Player 2 enter a number position from 0-8 to place your move: ");
+
+    }
+    while ((secondPlayerMove < 48 || secondPlayerMove > 56) && (boardPlace[i] == 'X' || boardPlace[i] == 'O'));
+
+    for(i = 0; i < 9; i++)
+    {
+        if(boardPlace[i] == secondPlayerMove )
+        {
+            boardPlace[i] = 'O';
+        }
+    }
+
+    letsPlay = drawBoard();
+
+
+
 
 
 
@@ -33,6 +70,8 @@ char drawBoard()
     int i;
     int j;
 
+    printf("\n");
+
     for (i = 0; i <= 4; i++)
     {
         for(j = 0; j < 1; j++)
@@ -40,19 +79,19 @@ char drawBoard()
 
             if (i == 0)
             {
-                printf("%c|%c|%c \n", boardPlace[0], boardPlace[1], boardPlace[2]);
+                printf("     %c|%c|%c \n", boardPlace[0], boardPlace[1], boardPlace[2]);
             }
             else if (i % 2 == 1)
             {
-                printf("-+-+-\n");
+                printf("     -+-+-\n");
             }
             else if (i == 2)
             {
-                printf("%c|%c|%c \n", boardPlace[3], boardPlace[4], boardPlace[5]);
+                printf("     %c|%c|%c \n", boardPlace[3], boardPlace[4], boardPlace[5]);
             }
             else if (i == 4)
             {
-                printf("%c|%c|%c \n", boardPlace[6], boardPlace[7], boardPlace[8]);
+                printf("     %c|%c|%c \n\n", boardPlace[6], boardPlace[7], boardPlace[8]);
             }
 
 
