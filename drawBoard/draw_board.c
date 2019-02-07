2f908a58543b777218a3dcd94b823abab05b229d
Rose Parker <parker.rose@ymail.com>
2019-02-07 23:20:17 +0000

    game over returns true if the board is full of X's or O's or false if it's not

--- drawBoard/draw_board.c
+++ drawBoard/draw_board.c
@@ -11,7 +11,7 @@ bool isGameover();
 bool didWin();
 
 
-char boardPlace[9] = {'0','1','2','3','4','5','6','7','8'};
+char boardPlace[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
 
 int exitGame = 0;
 
@@ -73,15 +73,19 @@ bool isValidMove(char move)
 bool isGameover()
 {
     int i = 0;
-
     bool endGame = false;
 
     for(i = 0; i < 9; i++)
     {
         //if boardPlace is equal to only alpha
-        if(isalpha(boardPlace[i]) && (!isalpha(boardPlace[i])))
+        if(boardPlace[i] == ' ')
         {
             printf("%i %c\n",i, boardPlace[i]);
+            endGame = false;
+            break;
+        }
+        else
+        {
             endGame = true;
         }
     }
